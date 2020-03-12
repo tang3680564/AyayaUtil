8c7d4dcddd0bde50ea65749f8f4f5ccfb1ce3444
tang3680564 <494326253@qq.com>
2020-03-12 10:59:31 +0800

    Initial Commit

---
+++ AyayaUtil/AyayaUtil.h
@@ -0,0 +1,19 @@
+//
+//  AyayaUtil.h
+//  AyayaUtil
+//
+//  Created by Ayaya on 2020/3/12.
+//  Copyright © 2020 Ayaya. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AyayaUtil.
+FOUNDATION_EXPORT double AyayaUtilVersionNumber;
+
+//! Project version string for AyayaUtil.
+FOUNDATION_EXPORT const unsigned char AyayaUtilVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AyayaUtil/PublicHeader.h>
+
+
